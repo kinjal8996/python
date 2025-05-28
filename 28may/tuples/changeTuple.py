7959a490735d3be502b7fab15016d84f45c594c6
kinjal <89kinjal@gmail.com>
2025-05-28 10:29:43 +0530

    tuple

---
+++ 28may/tuples/changeTuple.py
@@ -0,0 +1,6 @@
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[2] = "kiwi"
+x = tuple(y)
+
+print(x)
---
+++ 28may/tuples/createTuple.py
@@ -0,0 +1,7 @@
+# Create Tuple With One Item
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
