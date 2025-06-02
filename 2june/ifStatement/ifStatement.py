9b97c0d6231199741c4d1b150d9866d4fd7b1f08
kinjal <89kinjal@gmail.com>
2025-06-02 10:11:41 +0530

    loop

---
+++ 2june/ifStatement/ifStatement.py
@@ -0,0 +1,23 @@
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+# a = 33
+# b = 200
+
+# if b > a:
+#   print("b is greater than a")
+
+# And keyword
+# a = 200
+# b = 33
+# c = 500
+# if a > b and c > a:
+#   print("Both conditions are True")
+# or  keyword
+# a = 200
+# b = 33
+# c = 500
+# if a > b or a > c:
+#   print("At least one of the conditions is True")
+
---
+++ 2june/loops/loops.py
@@ -0,0 +1,26 @@
+# Python Loops
+# Python has two primitive loop commands:
+
+# while loops
+# for loops
+# The while Loop
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+
+# Python For Loops
+# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+
+# This is less like the for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
+
+# With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+
+# This loop is used to iterate over each item in the list fruits.
+
+# In each iteration, the variable x will hold one item from the list.
